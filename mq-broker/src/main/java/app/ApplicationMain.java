90200e1cea61d1c1df4390300d2af7ebe1792aaf
anshmehta <mehta.ansh1@northeastern.edu>
2024-11-23 23:28:02 -0500

    removed unwanted classes added for testing

--- mq-broker/src/main/java/app/ApplicationMain.java
+++ mq-broker/src/main/java/app/ApplicationMain.java
@@ -13,24 +13,5 @@ public class ApplicationMain {
   public static void main(String[] args) {
 
 
-    // read environment variables
-
-    int brokerId = Integer.parseInt(System.getenv(EnvironmentVars.BROKER_ID.getName()));
-    String brokerHostname = System.getenv(EnvironmentVars.BROKER_HOSTNAME.getName());
-    String peerHostnames = System.getenv(EnvironmentVars.PEER_HOSTNAMES.getName());
-    List<String> peers;
-    if (peerHostnames != null && !peerHostnames.isEmpty()) {
-      peers = Arrays.asList(peerHostnames.split(","));
-    }
-
-    // initialize the broker (bootstrap)
-
-
-
-
-
-
-
-
   }
 }
