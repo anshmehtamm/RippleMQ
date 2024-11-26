92b1163ff39358af2f509d609f53d3f39b45a898
Shail Shah <shailshah@shailm1air.local>
2024-11-26 18:47:19 -0500

    Added ProducerAPI Interface

---
+++ mq-common/src/main/java/messaging/ProducerAPI.java
@@ -0,0 +1,29 @@
+package messaging;
+
+/**
+ * Interface for producers to send messages to brokers using round-robin distribution.
+ */
+public interface ProducerAPI {
+
+  /**
+   * Produces a new message to the specified topic.
+   *
+   * @param topic The topic to which the message will be sent.
+   * @param message The message object to be sent.
+   */
+  void produce(Topic topic, Message message);
+
+  /**
+   * Connects to the list of brokers to which the producer will connect.
+   * Returns - The Acknowledgement of connection with the brokers
+   */
+  int connect();
+
+  /**
+   * Closes all open connections and releases resources.
+   */
+  void disconnect();
+
+
+
+}
